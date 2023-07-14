26c6b4595011ddb353adc22ace98e70d68ce208c
Deep <dp124551634@gmail.com>
2023-07-14 15:41:48 +0530

    Create templates for client and server side.

---
+++ client/src/Landing_page.js
@@ -0,0 +1,11 @@
+import './Landing_page.css';
+
+function LP() {
+  return (
+    <>
+      <h1>Hii!</h1>
+    </>
+  );
+}
+
+export default LP;
---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LP from './Landing_page';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <LP />
+  </React.StrictMode>
+);
