23916a4092e4b0eb7285e3d54522bdf0c4eecfd0
Deep <dp124551634@gmail.com>
2023-09-19 20:11:42 +0530

    Add repute button.

--- client/src/ProfileComponents/profiole_complete.js
+++ client/src/ProfileComponents/profiole_complete.js
@@ -5,21 +5,33 @@ import experience_icon from "../assets/images/icons/experience-icon.svg";
 import language_icon from "../assets/images/icons/language-icon.svg";
 import phone_icon from "../assets/images/icons/phone-icon.svg";
 import email_icon from "../assets/images/icons/email-icon.svg";
+import Whiteheart_icon from "../assets/images/miscellaneous/Whiteheart-svgrepo-com.svg";
+import Redheart_icon from "../assets/images/miscellaneous/Redheart-svgrepo-com.svg";
 import { useEffect, useState } from "react";
 
 function ProfileComplete({ username }) {
 
     let [dataArrived, setDataArrived] = useState(false);
+    let [heart, setHeart] = useState(Whiteheart_icon);
     let [doctor, setDoctor] = useState({});
+
+    function handleLike(e) {
+        if (heart === Whiteheart_icon) {
+            setHeart(Redheart_icon);
+        }
+        else{
+            setHeart(Whiteheart_icon);
+        }
+    }
 
     async function fetchInfo() {
         let info = await fetch(`http://localhost:4000/getProfile/${username}`, {
-            method:'GET',
+            method: 'GET',
             headers: { "Content-Type": "application/json" }
         })
-        .then(res =>{
-            return res.json();
-        })
+            .then(res => {
+                return res.json();
+            })
         setDoctor(info);
         setDataArrived(true);
     }
@@ -35,6 +47,7 @@ function ProfileComplete({ username }) {
                     <div className={styles.namespec}>
                         <div className={styles.name}>{doctor.username}</div>
                         <div className={styles.spec}>{doctor.specialization}</div>
+                        <button className={styles.likeBtn} onClick={(e) => handleLike(e)}><span>Repute </span><img src={heart} alt="Heart" style={{ width: "25px" }} /></button>
                     </div>
                 </div>
                 <div className={styles.profiletitle}>Profile</div>
